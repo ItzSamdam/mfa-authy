2b8f5259b12f4fc326ccd940c866a4fbdcde867b
Samuel Owadayo <odevservices@gmail.com>
2023-12-30 09:14:57 +0100

    adding uri-js dependency

--- src/otp.ts
+++ src/otp.ts
@@ -9,45 +9,33 @@ export function generateOtpKey(): Buffer {
     return key;
 }
 
-export function encodeGoogleAuthKey(bin: Buffer): string {
+export function encodeMFAAuthKey(bin: Buffer): string {
     const base32: string = b32.encode(bin).toString('utf8').replace(/=/g, '');
     const key: string = base32.toLowerCase().replace(/(\w{4})/g, '$1 ').trim();
     return key;
 }
 
-export function generateGoogleAuthKey(): string {
-    return encodeGoogleAuthKey(generateOtpKey());
+export function generateMFAAuthKey(): string {
+    return encodeMFAAuthKey(generateOtpKey());
 }
 
-export function decodeGoogleAuthKey(key: string): Buffer {
+export function decodeMFAAuthKey(key: string): Buffer {
     const unformatted: string = key.replace(/\W+/g, '').toUpperCase();
     const bin: Buffer = b32.decode(unformatted);
     return bin;
 }
 
-export function generateGoogleAuthToken(key: string): string {
-    const bin: Buffer = decodeGoogleAuthKey(key);
+export function generateMFAAuthToken(key: string): string {
+    const bin: Buffer = decodeMFAAuthKey(key);
     return notp.totp.gen(bin);
 }
 
-export function verifyGoogleAuthToken(key: string, token: string, options?: { window?: number; time?: number }): boolean {
-    const bin: Buffer = decodeGoogleAuthKey(key);
+export function verifyMFAAuthToken(key: string, token: string, option?: { window: number, time: number }): boolean {
+    const bin: Buffer = decodeMFAAuthKey(key);
 
-    token = token.replace(/\W+/g, '');
+    // window is +/- 1 period of 30 seconds
+    const verificationResult = notp.totp.verify(token, bin, { window: option?.window || 1, time: option?.time || 30 });
 
-    const verificationOptions = {
-        window: options?.window || 1,  // Use custom window if provided, otherwise default to 1
-        time: options?.time || 30      // Use custom time if provided, otherwise default to 30 seconds
-    };
-
-    // Verify with the custom options
-    const verificationResult = notp.totp.verify(token, bin, verificationOptions);
-
-    // If verification fails and a default fallback time is provided, try again with the default time
-    if (!verificationResult && verificationOptions.time !== 30) {
-        const defaultVerificationOptions = { window: verificationOptions.window, time: 30 };
-        return notp.totp.verify(token, bin, defaultVerificationOptions);
-    }
-
-    return verificationResult;
+    // Explicitly return a boolean value
+    return verificationResult !== null;
 }
--- src/uri.ts
+++ src/uri.ts
@@ -1,6 +1,6 @@
 // src/uri.ts
-import { encodeURI, encodeURIComponent } from 'uri-js';
-import { generateGoogleAuthKey } from './otp';
+import * as URI from 'uri-js';  // Import the entire module
+
 
 export function generateTotpUri(
     secret: string,
@@ -10,21 +10,20 @@ export function generateTotpUri(
     digits: number,
     period: number
 ): string {
+    const sanitizedSecret = secret.replace(/[\s\.\_\-]+/g, '').toUpperCase();
+    const encodedIssuer = URI.serialize(URI.parse(issuer || ''));  // Use URI.parse and URI.serialize for encoding
+    const encodedAccountName = URI.serialize(URI.parse(accountName || ''));  // Use URI.parse and URI.serialize for encoding
+    const algorithm = algo || 'SHA1';
+    const numDigits = digits || 6;
+    const timePeriod = period || 30;
+
     return (
-        'otpauth://totp/' +
-        encodeURI(issuer || '') +
-        ':' +
-        encodeURI(accountName || '') +
-        '?secret=' +
-        secret.replace(/[\s\.\_\-]+/g, '').toUpperCase() +
-        '&issuer=' +
-        encodeURIComponent(issuer || '') +
-        '&algorithm=' +
-        (algo || 'SHA1') +
-        '&digits=' +
-        (digits || 6) +
-        '&period=' +
-        (period || 30)
+        `otpauth://totp/${encodedIssuer}:${encodedAccountName}` +
+        `?secret=${sanitizedSecret}` +
+        `&issuer=${encodeURIComponent(issuer || '')}` +  // Use standard JavaScript encodeURIComponent
+        `&algorithm=${algorithm}` +
+        `&digits=${numDigits}` +
+        `&period=${timePeriod}`
     );
 }
 
